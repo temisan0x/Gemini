53024401f4caff6020ca0b18d13ccab4c5554ff2
Heytemisan <temyicy@gmail.com>
2021-12-15 12:10:04 +0100

    updated jsx files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import SimpleSlider from './components/SimpleSlider';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/components/SimpleSlider.jsx
+++ src/components/SimpleSlider.jsx
@@ -1,18 +1,46 @@
 import Slider from "react-slick";
-import React from "react";
+import React, { Component } from "react";
 
-const slider = [
+const names = [
     {
         name: "introducing the Gemini card"
     },
     {
-        name2: "introducing the Gemini card"
+        name: "introducing the Gemini card"
     },
     {
-        name3: "introducing the Gemini card"
+        name: "introducing the Gemini card"
     },
     {
-        name4: "introducing the Gemini card"
+        name: "introducing the Gemini card"
     }
 ]
+
+export default class SimpleSlider extends Component {
+    render() {
+        const settings = {
+        dots: true,
+        fade: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrow: true,
+        className:"slides"
+        };
+        return (
+            <div>
+            <Slider {...settings}>
+                {names.map((name)=> {
+                    return(
+                        <div>
+                            <li>{name}</li>
+                        </div>
+                    )
+                })}
+            </Slider>
+            </div>
+        );
+        }
+    }
 
