98bb0c9820fece1b9f8f9d4f8f9ce375d88eef7e
Heytemisan <temyicy@gmail.com>
2021-12-16 00:57:17 +0100

    updated Featured section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import SimpleSlider from './components/SimpleSlider';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Hero from './components/navbar/Hero';
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <div className="App">
         <Navbar/>  
-        {/* <SimpleSlider/> */}
         <Hero />
         <Featured/>
       </div>
