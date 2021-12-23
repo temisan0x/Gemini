161b15d85b02fd0babb8efa9463aae56e5ffc32e
Heytemisan <temyicy@gmail.com>
2021-12-23 22:09:02 +0100

    updated Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -3,10 +3,20 @@ import React, {useState, useEffect} from 'react'
 
 
 function Api() {
+    const [coins,setCoins]= useState([])
     
+    useEffect(()=> {
+        axios.get(" https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+        .then(res => {
+            setCoins(res.data);
+        }).catch(error => alert('error'))
+    }, [])
+
     return (
         <div>
-            
+            <div className="coin">
+                
+            </div>
         </div>
     )
 }
