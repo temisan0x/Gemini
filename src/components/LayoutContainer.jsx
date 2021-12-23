0ebb12e932fe5bfbfe262808004bd41103ea002f
Heytemisan <temyicy@gmail.com>
2021-12-23 12:39:25 +0100

    updated LayoutContainer.jsx

--- src/components/LayoutContainer.jsx
+++ src/components/LayoutContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link} from "react-router-dom"
 
 const Layout = styled.div `
     background-color: rgb(26, 28, 29);
@@ -7,6 +8,36 @@ const Layout = styled.div `
     padding-right:70px;
     padding-top: 70px;
     padding-bottom: 56px;
+
+    .layout_flex {
+        display: flex;
+        flex-direction: column;
+        line-height:5;
+        align-items:center;
+        justify-content:center;
+    }
+
+    p {
+        color:#ffffff;
+        font-weight:400px;
+    }
+
+    h2 {
+        color:#ffffff;
+        font-size: 36px;
+        line-height: 44px;
+        font-weight: 600;
+    }
+
+    .layout_btn {
+
+    }
+
+    .layout_btn:hover{
+        border-color: rgb(124, 234, 251);
+        background-color: rgb(124, 234, 251);
+        color: rgb(1, 3, 4);
+    }
 `
 
 
@@ -15,7 +46,12 @@ function LayoutContainer() {
         <Layout>
             <div className="layout_flex">
                 <h2>A simple, secure way to buy and sell cryptocurrency</h2>
-                <p></p>
+                <p>Trade bitcoin and other cryptos in 3 minutes.</p>
+                <Link>
+                    <button className="layout_btn">
+                        Get started
+                    </button>
+                </Link>
             </div>
         </Layout>
     )
