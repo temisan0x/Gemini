1675c98545fcce42e71a4e09ee080d184c7de8fe
Heytemisan <temyicy@gmail.com>
2021-12-28 12:35:33 +0100

    Updated Logo.js

--- src/components/priceTracker/Logo.js
+++ src/components/priceTracker/Logo.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Col, Container } from 'reactstrap';
 
  
 function Logo() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Container>
+                <Row>
+                    <Col className="text-center" md="12">
+                        <img src='' alt="logo"/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
