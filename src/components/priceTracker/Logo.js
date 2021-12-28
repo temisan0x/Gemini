db3cf83330afa34686cac9896ac83b8310581167
Heytemisan <temyicy@gmail.com>
2021-12-28 12:40:59 +0100

    Updated Logo.js

--- src/components/priceTracker/Logo.js
+++ src/components/priceTracker/Logo.js
@@ -5,7 +5,9 @@ import { Col, Container, Row } from 'reactstrap';
 import logo from '../../img/gemini.jpg'
 
 const Crypto = styled.div `
-
+    .logo {
+        width:200px;
+    }
 `
 
 function Logo() {
@@ -14,7 +16,7 @@ function Logo() {
             <Container>
                 <Row>
                     <Col className="text-center" md="12">
-                        <img src={logo} alt="logo"/>
+                        <img src={logo} alt="logo" className="logo"/>
                     </Col>
                 </Row>
             </Container>
